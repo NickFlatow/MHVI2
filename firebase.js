efd6fe69667da54373d1daf4e153c375c142a6d7
Nick <nick.flatow@gmail.com>
2019-01-12 14:10:10 -0600

    changed repos

---
+++ firebase.js
@@ -0,0 +1,111 @@
+(function() {
+  var config = {
+    apiKey: "AIzaSyDaPeaiTpAzQcJSV46IDaaElp0OeAGp7E8",
+    authDomain: "mhvi-15e2b.firebaseapp.com",
+    databaseURL: "https://mhvi-15e2b.firebaseio.com",
+    projectId: "mhvi-15e2b",
+    storageBucket: "mhvi-15e2b.appspot.com",
+    messagingSenderId: "630227463036"
+  };
+  firebase.initializeApp(config);
+// blah
+  $(document).ready(function(){
+    //search database for item
+    //ERRORS left shoes
+    $("#search").click(function(){
+        //grab string from searchName textbox
+        searchDatabase($('#searchName').val(),$('#object'));
+    });
+    //update quanitity of item
+    $('#btnUpdate').click(function(){
+      updateDatabase(String($('#searchName').val()),$('#txtUpdate').val());
+    });
+    $('#loginBtn').click(function(){
+      let usr = $('#userName').val();
+      let pwd = $('#password').val();
+
+      //authenticate login information
+      firebase.auth().signInWithEmailAndPassword(usr, pwd).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert("Error : " + errorMessage);
+      });
+      //check if user is logged in
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // alert(user.email);
+          // check which user signed in and redirect accordingly.
+          if (user.email == 'admin@mhvi.com'){
+            window.location.href = "admin.html";
+          }
+          else if (user.email == 'driver@mhvi.com'){
+            window.location = "driver.html";
+          }
+        }
+      //onAuthStateChanged
+      });
+    //login button
+    });
+    $('#logoutBtn').click(function(){
+      firebase.auth().signOut();
+      window.location = "login.html";
+    });
+    const dbTable = firebase.database().ref();
+    dbTable.startAt('A').orderByKey().on('child_added', snap => {
+        $('#tableBody').append('<tr name ='+snap.key +'><td class="item"><span>' + snap.key + '</span></td><td class = "cost">' + snap.val().Cost + '</td><td class = quantity>' + snap.val().Quantity +
+            '<td><button type = "button" class="update">Update</button></td></tr>' +
+            '<tr class = "hide">'+
+              '<td><button type = "button" class = "delete">Delete</button></td>'+
+              '<td class = "hidden"><input type="text" name="costTR" placeholder="Cost"></td>'+
+              '<td class = "hidden"><input type="text" name="quantityTR" placeholder="Quantity"></td>'+
+              '<td><button type = "button" class = "submit">Submit</button>'+
+            '</td></tr>');
+          });
+    dbTable.on('child_changed', snap => {
+        $('tr[name =' + snap.key +']').find(".cost").html(snap.val().Cost);
+        $('tr[name =' + snap.key +']').find(".quantity").html(snap.val().Quantity);
+    });
+    dbTable.on('child_removed', snap => {
+        let $nextRow = $('tr[name = '+ snap.key +']').next("tr");
+        $nextRow.remove();
+        $('tr[name = '+ snap.key +']').remove();
+    });
+    //sync database changes in dropdown menu
+    //grab reference to database
+    const dbDropdown = firebase.database().ref();
+    //databse event handlers
+      //add all itmes from databases
+      dbDropdown.on('child_added', snap => {
+        //driver list
+        // console.log(snap.key.replace(/ /g,''));
+        $('#list').append('<option value = ' + snap.key + '>' + snap.val().Item + '</option>');
+        $('#adminList').append('<option value = ' + snap.key + '>' + snap.val().Item + '</option>');
+      })
+      // listens for changes to any child in the database
+      dbDropdown.on('child_changed', snap => {
+        $('#list option[value='+ snap.key+']').text(snap.val().Item);
+        $('#adminList option[value='+ snap.val().Item+']').text(snap.val().Item);
+      })
+      //listens for any children removed from the database then updates the selectlist
+      dbDropdown.on('child_removed', snap => {
+        $('#list option[value='+ snap.key +']').remove();
+        $('#adminList option[value='+snap.key +']').remove();
+      })
+
+    $('#list').change(function() {
+      let listItem = $('#list').find(":selected").text();
+      if (listItem != "Select Item"){
+        searchDatabase(listItem,$(driverOutput));
+      }
+    });
+    $('#adminList').change(function() {
+      let listItem = $('#adminList').find(":selected").text();
+      if (listItem != "Select Item"){
+        searchDatabase(listItem,$(output));
+      }
+    });
+    //doc.ready()
+    });
+//function
+}());
---
+++ validate.js
@@ -0,0 +1,134 @@
+$(function() {
+  //custom method to check that fields only contain letters and spaces
+  jQuery.validator.addMethod("lettersonly", function(value, element) {
+    return this.optional(element) || /^[a-z\s]+$/i.test(value);
+  }, "Only alphabetical characters and spaces");
+  jQuery.validator.addClassRules('hiddenTr', {
+          // required: true,
+          lettersonly:true
+    });
+  $("form[name='mhvi']").validate({
+    // Specify validation rules
+    rules: {
+      item: {
+        required: true,
+        // lettersonly:true
+      },
+      quantity:{
+        required: true,
+        digits: true,
+        min:0
+      },
+      cost:{
+        required:true,
+        number:true,
+        min:0
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      item: {
+        required:"Please enter an Item",
+      },
+      weight: {
+        required: "Please enter your weight",
+        digits: "you must enter positive numbers",
+        min: "your weight must be at least 2 digits"
+      }
+    },
+    //don't use parentheses when calling the function
+    submitHandler: dbWrite
+  });
+  $("#driver").validate({
+      rules: {
+        list: {
+          required: true
+        },
+        driverTxtUpdate:{
+          required: true,
+          range:[-100,100]
+        }
+      },
+      messages: {
+        list: {
+          required: "Please select an option from the dropdown menu"
+        },
+        driverTxtUpdate:{
+          required: "Please enter the quantity"
+        }
+      },
+  });
+  $("#adminSelectForm").validate({
+      rules: {
+        adminList: {
+          required: true
+        },
+        adminSelectQuantity:{
+          required: true,
+          range:[-100,100]
+        }
+      },
+      messages: {
+        adminList: {
+          required: "Please select an option from the dropdown menu"
+        },
+        adminSelectQuantity:{
+          required: "Please enter the quantity"
+        }
+      },
+      submitHandler: adminSelectForm
+  });
+  $('#table').validate({
+    rules:{
+      costTR:{
+        number:true
+      },
+      quantityTR:{
+        number:true
+      }
+    }
+  });
+  $('#driverDropOff').click(function() {
+    //check if driver form meets validation rules
+    if ($('#driver').valid()) {
+      let item = $('#list').find(":selected").text();
+      let quantity = $('#driverTxtUpdate').val();
+      updateDatabase(item,(-1 * quantity));
+    }
+  });
+  $('#driverPickUp').click(function() {
+    //check if driver form meets validation rules
+    if ($('#driver').valid()) {
+      let item = $('#list').find(":selected").text();
+      let quantity = $('#driverTxtUpdate').val();
+      updateDatabase(item,quantity);
+    }
+  });
+  $('#tableBody').on('click','.update',function(){
+    let $row = $(this).closest("tr");   // Find the row
+    let $nextRow = $row.next("tr"); // Find the next row down
+    $nextRow.toggle(); //show hide hidden row
+  });
+  $('#tableBody').on('click','.submit',function(){
+    let $row = $(this).closest("tr"); // get the row where the button was pushed
+    let $prevRow = $row.prev(); // get the previous row
+    let item = $prevRow.find(".item").text(); // Find the item we are upadting
+    let cost = $row.find("input[name = 'costTR']").val(); //Find textbox for Cost
+    let quantity = $row.find("input[name = 'quantityTR']").val(); //Find textbox for Quantity
+
+    if ($('#table').valid()) { //check if row meet validation requirments
+      adminUpdateDatabase(item,quantity,cost);
+    }
+  });
+  $('#tableBody').on('click','.delete',function(){
+    let $row = $(this).closest('tr');
+    let $item = $row.prev().find(".item").text();
+    const dbItem = firebase.database().ref($item);
+
+    if (confirm("Are you sure you want to delete " + $item + "?")){
+      dbItem.remove();
+    }
+
+  });
+//function
+});
