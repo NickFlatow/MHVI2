45ab971349e93dc717dac029d4d899463292b3ba
Nick <nick.flatow@gmail.com>
2019-01-13 23:01:20 -0600

    fixed size issue with admin table

--- firebase.js
+++ firebase.js
@@ -53,7 +53,7 @@
     const dbTable = firebase.database().ref();
 
     dbTable.startAt('A').orderByKey().on('child_added', snap => {
-        $('#tableBody').append('<tr name ='+snap.key +'><td class="item"><span>' + snap.key + '</span></td><td class = "cost">' + snap.val().Cost + '</td><td class = quantity>' + snap.val().Quantity + '</td>'+
+        $('#tableBody').append('<tr name ='+snap.key +'><td class="item" width = "254"><span>' + snap.key + '</span></td><td class = "cost" width ="531">' + snap.val().Cost + '</td><td class = quantity width ="531"> ' + snap.val().Quantity + '</td>'+
             '<td><button type = "button" class="update btn btn-primary">Update</button></td></tr>' +
             '<tr class = "hide bg-primary">'+
               '<td><button type = "button" class = "delete btn btn-danger">Delete</button></td>'+
